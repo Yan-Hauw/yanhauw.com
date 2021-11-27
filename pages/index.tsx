6c03d0531032afc22316c3bd081b3e18b9dc0351
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2021-11-26 18:42:42 -0800

    built basic css structure of profile box

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 // types
 import type { NextPage } from "next";
 
+//images
+import Bottle from "../public/Bottle.jpeg";
+
+// next
+import Image from "next/image";
+
 // components
 import NextHead from "../components/NextHead";
 
@@ -9,9 +15,18 @@ import styles from "../styles/index.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <NextHead />
-    </div>
+      <div className={styles.container}>
+        <div className={styles.photoFrame}>
+          <Image src={Bottle} alt="" height={78} width={78} />
+        </div>
+        <div className={styles.profileText}>
+          <h1 className={styles.fullName}>Yan Hauw</h1>
+          <p className={styles.description}>Junior CS major at UCLA</p>
+        </div>
+      </div>
+    </>
   );
 };
 
