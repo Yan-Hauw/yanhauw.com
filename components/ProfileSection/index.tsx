4efbc67e996b747a5f4613d93dd6c221edc52684
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2021-12-09 18:27:44 -0800

    removed -tw prefix, got tailwind for profile section working

--- components/ProfileSection/index.tsx
+++ components/ProfileSection/index.tsx
@@ -11,22 +11,26 @@ const externaImageLoader = ({ src }: { src: string }) =>
 
 const ProfileSection = () => {
   return (
-    <div id="profilesection" className={styles.container}>
-      <div className={styles.profileBox}>
-        <div className={styles.photoFrame}>
-          <Image
+    <div
+      id="profilesection"
+      className="h-screen flex items-center justify-center"
+    >
+      <div className="border-gray-300 border-2 rounded-md h-24 w-80 flex p-1.5">
+        <div className="h-20 w-20 bg-purple-600 rounded-full overflow-hidden">
+          {/* <Image
             src="Bottle.jpeg"
             loader={externaImageLoader}
             alt=""
+            layout="responsive"
             height={78}
             width={78}
-          />
+          /> */}
         </div>
 
-        <div className={styles.profileText}>
-          <h1 className={styles.fullName}>Yan Hauw</h1>
+        <div className="mx-3">
+          <h1 className="text-3xl font-semibold mb-2">Yan Hauw</h1>
 
-          <p className={styles.description}>Junior CS major at UCLA.</p>
+          <p className="text-sm">Junior CS major at UCLA.</p>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  prefix: "tw-",
   plugins: [],
 };
