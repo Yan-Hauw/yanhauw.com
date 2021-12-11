a1a92fe1e972e495d8d21f33464e415657bdaa30
Yan-Hauw <91395373+Yan-Hauw@users.noreply.github.com>
2021-12-10 17:19:29 -0800

    styled basic NavBar in Tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,15 @@ import type { NextPage } from "next";
 
 // components
 import NextHead from "../components/NextHead";
+import NavBar from "../components/NavBar";
 import ProfileSection from "../components/ProfileSection";
 import ProjectsSection from "../components/ProjectsSection";
 
 const Home: NextPage = () => {
   return (
     <>
+      <NavBar />
+
       <NextHead />
 
       <ProfileSection />
